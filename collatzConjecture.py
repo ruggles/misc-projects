0fbdeee5f99c16765caab1b000417fe66c85ca48
ruggles <iaob1408@gmail.com>
2014-05-09 11:09:25 -0700

    Added iterative Collatz function, a function that tests the conjecture over a range of numbers, and a function which graphs a single Collatz path.

--- collatzConjecture.py
+++ collatzConjecture.py
@@ -8,6 +8,9 @@ If n is odd, multiply it by 3 and add 1 to obtain 3n + 1. Repeat the process ind
 The conjecture is that no matter what number you start with, you will always eventually reach 1.
 """
 
+import pylab
+
+# Note that python's default maximum recursion depth is 1000
 
 def collatzRecursive(positiveInt, toPrint):
     # positiveInt only needs to be an integer in the 
@@ -21,13 +24,66 @@ def collatzRecursive(positiveInt, toPrint):
     if positiveInt == 1:
         return 1
     elif positiveInt%2 == 0:
-        return collatzSim(positiveInt/2, toPrint)
+        return collatzRecursive(positiveInt/2, toPrint)
     else:
-        return collatzSim(positiveInt*3 + 1, toPrint)
+        return collatzRecursive(positiveInt*3 + 1, toPrint)
+
+def collatzIterative(positiveInt, toPrint):
+    """
+    Input: positiveInt - Postitive integer or long
+    toPrint - if True, prints path as it's being taken
+    
+    Output: List showing the convergence path
+    """        
+    assert positiveInt > 0
+    assert type(positiveInt) == int or type(positiveInt) == long
+    
+    convergencePath = []
+    
+    while positiveInt != 1:
         
+        convergencePath.append(positiveInt)
+        
+        if toPrint:
+            print positiveInt
+            
+        if positiveInt%2 == 0:
+            positiveInt /= 2
+        else:
+            positiveInt = positiveInt*3 + 1
+            
+    return convergencePath
+    
+def collatzTest(numList, debug):
+    """
+    Input: numList - List of positive integers or longs
+        debug - prints number trajectory mid-program
+    Output: convergenceList - List of tuples (num, convergence time)
+    """
+    
+    assert type(numList) == list
+    convergenceList = []
+    
+    for i in numList:
+        convergenceList.append((i, len(collatzIterative(i, False))))
+        if debug:
+            print convergenceList[-1]
+        
+    return convergenceList
+    
+def collatzPathGraph(positiveInt):
+    """
+    Graphs the path the number takes on the y-axis
+    with the step number on the x-axis
+    
+    Input: positiveInt - Starting point for collatz path
+    """
+    
+    pylab.plot(collatzIterative(positiveInt, False))
+    pylab.title("Collatz Path for %s" % positiveInt)
+    pylab.ylabel("Number at Collatz step")
+    pylab.xlabel("Collatz step")
+    pylab.show()
+    
 # Function TODO List
-# Collatz Simulator which returns a list containing 
-#   the number's path
-# Test conjecture for large range of numbers
 # Graph convergence times for multiple numbers in a series
-# Graph convergence path for a single number
