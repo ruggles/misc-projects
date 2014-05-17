3c6cfd2b956a7b19820fcb2e131c1fd7796a1bda
ruggles <iaob1408@gmail.com>
2014-05-17 15:42:52 -0700

    Typed out basic outline for paint program

---
+++ rugPaint.py
@@ -0,0 +1,28 @@
+# Pro/g/ramming challenge # 83
+# Simple paint program
+
+def isFile(fileName):
+    pass
+    
+def openImage(fileName):
+    pass
+    
+def createImage(fileName):
+    pass
+    
+def renderImage(imageObject):
+    pass
+    
+def mouseInput(sysInput):
+    pass
+    
+class paintCursor(object):
+    def __init__(self):
+        pass
+    def color(self):
+        pass
+    def draw(self, imageObject, (x,y)):
+        pass
+        
+def saveFile(imageObject):
+    pass
