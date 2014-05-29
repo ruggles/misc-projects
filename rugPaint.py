f4e7cd1aa3ba009295d051c7556748cf13c230e8
ruggles <iaob1408@gmail.com>
2014-05-29 04:02:11 -0700

    Added ability to open images

--- rugPaint.py
+++ rugPaint.py
@@ -3,36 +3,22 @@
 import pygame, sys
 from pygame.locals import *
 
-def isFile(fileName):
-    pass
-    
-def openImage(fileName):
-    pass
-    
-def createImage(fileName):
-    pass
-    
-def renderImage(imageObject):
-    pass
-    
-def mouseInput(sysInput):
-    pass
-    
-class paintCursor(object):
-    def __init__(self):
-        pass
-    def color(self):
-        pass
-    def draw(self, imageObject, (x,y)):
-        pass
-        
-def saveFile(imageObject):
-    pass
-    
 def main():
     pygame.init()
     DISPLAYSURF = pygame.display.set_mode((800, 600))
     pygame.display.set_caption('Ruggles Paint')
+    
+    try:
+        imageName = sys.argv[1]
+    except IndexError:
+        print "Please supply a name for the image file"
+        raise IndexError
+        
+    try:
+        imgSurf = pygame.image.load(imageName)
+        DISPLAYSURF.blit(imgSurf, (0,0))
+    except pygame.error:
+        print "Image does not exist"
     
     RED = (255, 0, 0)
     GREEN = (0, 255, 0)
@@ -42,9 +28,10 @@ def main():
     while True:
         for event in pygame.event.get():
         
-            # if rmb is pushed    
+            # if rmb is pushed, save image
             if event.type == MOUSEBUTTONDOWN and event.button == 3:
-                pygame.image.save(DISPLAYSURF,"testshot.png")
+                pygame.image.save(DISPLAYSURF, imageName)
+                print "Image Saved", imageName
             
             # if lmb is held down
             if pygame.mouse.get_pressed()[0]:
