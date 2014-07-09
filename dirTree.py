50810ca2c492d6d2accfa781987467c3d0f684cf
ruggles <iaob1408@gmail.com>
2014-07-09 08:46:52 -0700

    Added command line support

--- dirTree.py
+++ dirTree.py
@@ -1,8 +1,14 @@
 # Python Directory Tree Builder
 
 import os
+import sys
 
-def buildTree(treeName, directory):
+def buildTree(directory, treeName):
+
+	"""
+	directory: Root directory for the tree.
+	treeName: Name of the resulting file.
+	"""
 	
 	treeFile = open(treeName, 'w')
 	
@@ -14,3 +20,7 @@ def buildTree(treeName, directory):
 		
 	treeFile.close()
 	
+if __name__ == "__main__":
+
+	# argv[1] is the directory, argv[2] is the saved filename
+	buildTree(sys.argv[1], sys.argv[2])
